b3825d02bb80e88aa3920a33977d4df73782f81b
fizzahhashmi-10p <fizzah.hashmi@10pearls.com>
2024-07-12 08:45:12 +0500

    Send Notifications events for author related actions

--- src/main/kotlin/com/demo/kafka/consumer/KafkaConsumer.kt
+++ src/main/kotlin/com/demo/kafka/consumer/KafkaConsumer.kt
@@ -4,6 +4,8 @@ import com.demo.util.Constants.COURSE_GROUP_ID
 import com.demo.util.Constants.COURSE_ADDED_TOPIC
 import com.demo.util.Constants.COURSE_DELETED_TOPIC
 import com.demo.util.Constants.COURSE_UPDATED_TOPIC
+import com.demo.util.Constants.USER_ADDED_TOPIC
+import com.demo.util.Constants.USER_GROUP_ID
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
@@ -13,7 +15,11 @@ class KafkaConsumer {
     private val logger = LoggerFactory.getLogger(this.javaClass)
     @KafkaListener(topics = [COURSE_ADDED_TOPIC, COURSE_DELETED_TOPIC, COURSE_UPDATED_TOPIC], groupId = COURSE_GROUP_ID)
     fun CourseListener(message: String) {
-        logger.info("Message received: [$message]")
+        logger.info("Course Notification Received: [$message]")
     }
 
+    @KafkaListener(topics = [USER_ADDED_TOPIC], groupId = USER_GROUP_ID)
+    fun UserListener(message: String) {
+        logger.info("User  Notification Received: [$message]")
+    }
 }
--- src/main/kotlin/com/demo/util/Constants.kt
+++ src/main/kotlin/com/demo/util/Constants.kt
@@ -5,4 +5,7 @@ object Constants {
     const val COURSE_UPDATED_TOPIC = "course-updated"
     const val COURSE_DELETED_TOPIC = "course-deleted"
     const val COURSE_GROUP_ID = "course-notification-consumer-group"
+
+    const val USER_ADDED_TOPIC = "user-added"
+    const val USER_GROUP_ID = "user-notification-consumer-groupp"
 }
